59bd8d7aa0ceda1f80b70135beb01de2072d8758
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-05-26 18:40:44 +0100

    handled cast error and validation error

--- middleWare/errorHandler.js
+++ middleWare/errorHandler.js
@@ -1,9 +1,22 @@
 const errorHandler = (err, req, res, next) => {
+  //handle duplicate unique values
   if (err.code === 11000) {
     return res.status(400).json({
       msg: `${Object.keys(err.keyValue)}(${Object.values(
         err.keyValue
       )}) is taken. Please Choose another`,
+    });
+  }
+  //handle cast error
+  if (err.name === "CastError") {
+    return res.status(404).json({
+      msg: `${err.value} does not exist in database`,
+    });
+  }
+  //handle validation error
+  if (err.name === "ValidationError") {
+    return res.status(401).json({
+      msg: `${err.message}`,
     });
   }
   return res.status(500).json({ msg: err });
